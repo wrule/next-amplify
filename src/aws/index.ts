04c8a4432d03c5a8b3e4c7d7a2c4d85dcaf6bac9
jimao <shihao.gu@perfma.com>
2024-10-30 00:49:52 +0800

    fix

--- src/aws/index.ts
+++ src/aws/index.ts
@@ -2,9 +2,9 @@ import { Amplify, ResourcesConfig } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/api';
 
 const str1 = "{\"API\":{\"GraphQL\":{\"endpoint\":\"https://kj4au2owkrakxkfsfx2lvpn3ny.appsync-api.ap-northeast-1.amazonaws.com/graphql\",\"region\":\"ap-northeast-1\",\"defaultAuthMode\":\"apiKey\",\"apiKey\":\"da2-ccctlxdlbzgurdu3brtr722xly\"}}}";
-const str2 = (process.env.app_aws_exports as string).slice();
+const str2 = (process.env.app_aws_exports as string)?.slice();
 
-console.log(1234, str1 === str2);
+console.log(1234,  Buffer.from(str1).toString('hex') === Buffer.from(str2).toString('hex'));
 
 const config = (process.env.NODE_ENV !== 'production' ?
   require('../../aws_config/aws-exports').default :
