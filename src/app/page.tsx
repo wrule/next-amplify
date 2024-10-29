e3f4ed5b9d36823937f607ab10f22235f7b208c3
jimao <shihao.gu@perfma.com>
2024-10-29 17:22:00 +0800

    Amplify.configure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
+import { Amplify, ResourcesConfig } from 'aws-amplify';
+import config from '../../aws_config/aws-exports';
+
+Amplify.configure(config as ResourcesConfig);
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
