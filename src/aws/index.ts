cc08a2bdb574ea7d95a3f687bc957eeb8f3d0e33
jimao <shihao.gu@perfma.com>
2024-10-29 23:45:46 +0800

    false

--- src/aws/index.ts
+++ src/aws/index.ts
@@ -1,9 +1,11 @@
 import { Amplify, ResourcesConfig } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/api';
 
-const config = (process.env.NODE_ENV !== 'production' ?
+const config = (false ?
   require('../../aws_config/aws-exports').default :
-  JSON.parse(process.env.app_aws_exports as string)
+  JSON.parse(
+    ("{\"API\":{\"GraphQL\":{\"endpoint\":\"https://kj4au2owkrakxkfsfx2lvpn3ny.appsync-api.ap-northeast-1.amazonaws.com/graphql\",\"region\":\"ap-northeast-1\",\"defaultAuthMode\":\"apiKey\",\"apiKey\":\"da2-ccctlxdlbzgurdu3brtr722xly\"}}}" as string)
+  )
 ) as ResourcesConfig;
 console.log(1234, config);
 
