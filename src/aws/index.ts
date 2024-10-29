f8a89c51709850b689dd295766a88028440521bd
jimao <shihao.gu@perfma.com>
2024-10-30 00:14:05 +0800

    9292

--- src/aws/index.ts
+++ src/aws/index.ts
@@ -4,14 +4,13 @@ import { generateClient } from 'aws-amplify/api';
 const config = (process.env.NODE_ENV !== 'production' ?
   require('../../aws_config/aws-exports').default :
   JSON.parse(
-    process.env.app_aws_exports as string
-    // ("{\"API\":{\"GraphQL\":{\"endpoint\":\"https://kj4au2owkrakxkfsfx2lvpn3ny.appsync-api.ap-northeast-1.amazonaws.com/graphql\",\"region\":\"ap-northeast-1\",\"defaultAuthMode\":\"apiKey\",\"apiKey\":\"da2-ccctlxdlbzgurdu3brtr722xly\"}}}" as string)
+    // process.env.app_aws_exports as string
+    ("{\"API\":{\"GraphQL\":{\"endpoint\":\"https://kj4au2owkrakxkfsfx2lvpn3ny.appsync-api.ap-northeast-1.amazonaws.com/graphql\",\"region\":\"ap-northeast-1\",\"defaultAuthMode\":\"apiKey\",\"apiKey\":\"da2-ccctlxdlbzgurdu3brtr722xly\"}}}" as string)
   )
 ) as ResourcesConfig;
-console.log(1234, config);
-console.log(5678, process.env.app_aws_exports, JSON.stringify(process.env.app_aws_exports), typeof process.env.app_aws_exports);
-console.log(4321, JSON.parse(process.env.app_aws_exports as string));
-Amplify.configure(config);
+console.log(1234, "{\"API\":{\"GraphQL\":{\"endpoint\":\"https://kj4au2owkrakxkfsfx2lvpn3ny.appsync-api.ap-northeast-1.amazonaws.com/graphql\",\"region\":\"ap-northeast-1\",\"defaultAuthMode\":\"apiKey\",\"apiKey\":\"da2-ccctlxdlbzgurdu3brtr722xly\"}}}");
+console.log(5678, process.env.app_aws_exports);
+console.log(9292, process.env.app_aws_exports === "{\"API\":{\"GraphQL\":{\"endpoint\":\"https://kj4au2owkrakxkfsfx2lvpn3ny.appsync-api.ap-northeast-1.amazonaws.com/graphql\",\"region\":\"ap-northeast-1\",\"defaultAuthMode\":\"apiKey\",\"apiKey\":\"da2-ccctlxdlbzgurdu3brtr722xly\"}}}");
 
 const client = generateClient();
 
