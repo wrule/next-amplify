e5ec935535b9c6bc112d22e34ebc783559da1240
jimao <shihao.gu@perfma.com>
2024-10-29 20:51:24 +0800

    fix: bug

--- src/app/api/env/route.ts
+++ src/app/api/env/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export
-async function GET(request: Request) {
+async function GET() {
   return NextResponse.json({
     env: JSON.stringify(process.env),
   });
