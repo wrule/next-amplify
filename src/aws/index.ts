2f9bbb8167e48acb40556229712f90f38877a04a
jimao <shihao.gu@perfma.com>
2024-10-29 21:02:12 +0800

    fix

--- src/aws/index.ts
+++ src/aws/index.ts
@@ -1,10 +1,13 @@
 import { Amplify, ResourcesConfig } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/api';
 
-Amplify.configure((process.env.NODE_ENV !== 'production' ?
+const config = (process.env.NODE_ENV !== 'production' ?
   require('../../aws_config/aws-exports').default :
   JSON.parse(process.env.app_aws_exports as string)
-) as ResourcesConfig);
+) as ResourcesConfig;
+console.log(1234, config);
+
+Amplify.configure(config);
 
 const client = generateClient();
 
