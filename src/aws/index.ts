f38a353de1c73fa325993ea9b0df2fb0bf94d8ac
jimao <shihao.gu@perfma.com>
2024-10-30 11:08:13 +0800

    json2

--- src/aws/index.ts
+++ src/aws/index.ts
@@ -7,7 +7,7 @@ const jsonText2 = process.env.NEXT_APP_CONFIG as string;
 console.log(1234, jsonText1 === jsonText2);
 
 const env = { ...process.env };
-const config = JSON.parse(jsonText2 === undefined ? jsonText1 : jsonText2) as ResourcesConfig;
+const config = JSON.parse(jsonText2) as ResourcesConfig;
 
 Amplify.configure(config);
 const client = generateClient();
