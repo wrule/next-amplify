8e28aeb15dfe96e5d881911ab68cfeb2ce5642b2
jimao <shihao.gu@perfma.com>
2024-10-30 10:24:52 +0800

    jsonText2

--- src/aws/index.ts
+++ src/aws/index.ts
@@ -2,11 +2,11 @@ import { Amplify, ResourcesConfig } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/api';
 
 const jsonText1 = "{\"API\":{\"GraphQL\":{\"endpoint\":\"https://kj4au2owkrakxkfsfx2lvpn3ny.appsync-api.ap-northeast-1.amazonaws.com/graphql\",\"region\":\"ap-northeast-1\",\"defaultAuthMode\":\"apiKey\",\"apiKey\":\"da2-ccctlxdlbzgurdu3brtr722xly\"}}}";
-const jsonText2 = process.env.app_aws_exports;
+const jsonText2 = process.env.app_aws_exports as string;
 
 console.log(1234, jsonText1 === jsonText2);
 
-const config = JSON.parse(jsonText1) as ResourcesConfig;
+const config = JSON.parse(jsonText2) as ResourcesConfig;
 
 Amplify.configure(config);
 const client = generateClient();
