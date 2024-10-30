2bc44d9c1b7b5c6b4bc54121d328d125f30da640
jimao <shihao.gu@perfma.com>
2024-10-30 09:54:13 +0800

    fix

--- src/aws/index.ts
+++ src/aws/index.ts
@@ -1,15 +1,10 @@
 import { Amplify, ResourcesConfig } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/api';
 
-const str1 = "{\"API\":{\"GraphQL\":{\"endpoint\":\"https://kj4au2owkrakxkfsfx2lvpn3ny.appsync-api.ap-northeast-1.amazonaws.com/graphql\",\"region\":\"ap-northeast-1\",\"defaultAuthMode\":\"apiKey\",\"apiKey\":\"da2-ccctlxdlbzgurdu3brtr722xly\"}}}";
-const str2 = (process.env.app_aws_exports as string)?.slice();
+const jsonText1 = "{\"API\":{\"GraphQL\":{\"endpoint\":\"https://kj4au2owkrakxkfsfx2lvpn3ny.appsync-api.ap-northeast-1.amazonaws.com/graphql\",\"region\":\"ap-northeast-1\",\"defaultAuthMode\":\"apiKey\",\"apiKey\":\"da2-ccctlxdlbzgurdu3brtr722xly\"}}}";
+const jsonText2 = "";
 
-console.log(1234,  Buffer.from(str1).toString('hex') === Buffer.from(str2).toString('hex'));
-
-const config = (process.env.NODE_ENV === 'production' ?
-  require('../../aws_config/aws-exports').default :
-  JSON.parse(str1)
-) as ResourcesConfig;
+const config = JSON.parse(jsonText1) as ResourcesConfig;
 
 Amplify.configure(config);
 const client = generateClient();
